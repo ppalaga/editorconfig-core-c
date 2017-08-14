5b1110fcf0788791d537952787bf02aa797e3c21
Michael Kriese <michael.kriese@visualon.de>
2017-08-14 11:15:11 +0200

    Added Windows build scripts
    Squashed from https://github.com/editorconfig/editorconfig-core-c/pull/29

---
+++ test.ps1
@@ -0,0 +1,39 @@
+param(
+    [ValidateSet("pcre","core")]
+    [string] $proj = "core",
+    
+    [ValidateSet("ON","OFF")]
+    [string] $static = "ON",
+
+    [ValidateSet("Debug","Release")]
+    [string] $config = "Release",
+
+    [ValidateSet("x86","x64")]
+    [string] $arch = "x64"
+)
+
+$ErrorActionPreference="stop"
+
+
+$dest = "bin"
+
+if ((Test-Path $dest) -ne $true){
+    throw "Missing build path! Used init?"
+}
+
+$dest += "\$arch"
+
+if($static -eq "ON"){
+    $dest += "-static"
+}
+
+$dest += "\$proj"
+
+if ((Test-Path $dest) -ne $true){
+    throw "Missing build path! Used init?"
+}
+
+$env:Path += ";" + (Resolve-Path bin\$arch\build\bin)
+
+"Testing $proj" | Write-Host -ForegroundColor DarkGreen
+cmake --build $dest --target run_tests `-- /p:Configuration=$config
